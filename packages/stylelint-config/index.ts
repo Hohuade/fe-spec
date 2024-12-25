b266b013c914429cf1e77049167aa3ad9ba10ab9
lianghd01 <lianghd01@mingyuanyun.com>
2024-12-25 10:23:03 +0800

    feat: stylelint

---
+++ packages/stylelint-config/index.ts
@@ -0,0 +1,11 @@
+module.exports = {
+	extends: [
+		'stylelint-config-standard',
+		'stylelint-config-prettier',
+		'stylelint-config-recess-order',
+		'stylelint-config-recommended-vue/scss',
+		'stylelint-config-recommended-vue',
+		'stylelint-config-recommended',
+		'stylelint-config-standard-scss',
+	],
+}
